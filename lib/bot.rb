9a61f8ae08aed83cd92896b310cccc9cdbe5f382
Delice Lydia <Delydia84@gmail.com>
2020-11-16 11:55:44 +0200

    Add learn Api

--- lib/bot.rb
+++ lib/bot.rb
@@ -1,25 +1,37 @@
-require 'telegram/bot'
-require_relative 'motive.rb'
+require "telegram/bot"
+require_relative "motive.rb"
 
 class Bot
   def initialize
-    token = '1482463910:AAEhuRP9w5nbNq7-ia3nx9FlEdkqlbtWtQQ'
+    token = "1482463910:AAEhuRP9w5nbNq7-ia3nx9FlEdkqlbtWtQQ"
     Telegram::Bot::Client.run(token) do |bot|
       bot.listen do |message|
         case message.text
 
-        when '/start'
-          bot.api.send_message(chat_id: message.chat.id, text: "Hello, #{message.from.first_name} , welcome to motivation chat bot created by peter robert, the chat bot is to keep you motivated and entertained. 
-                    Use  /start to start the bot,  /stop to end the bot, /motivate to get a diffrent motivational quote everytime you request for it or 
-                    /joke to get a joke everytime you request for it")
-        when '/stop'
+        when "/start"
+          bot.api.send_message(chat_id: message.chat.id, text: "Hello, #{message.from.first_name} , welcome to motivation chat bot created by Delice Lydia, the chat bot is to keep you motivated and entertained. 
+                    Use /start to start the bot,  /stop to end the bot, /motivate to get a different motivational quote everytime you request for it or 
+                    /learn to learn a new french word")
+        when "/learn"
+          new_word = %w[comment,tu,vas?]
+          bot.api.send_message(chat_id: message.chat.id, text: "#{new_word.sample.capitalize}!", date: message.date)
+
+        when "/stop"
           bot.api.send_message(chat_id: message.chat.id, text: "Bye, #{message.from.first_name}", date: message.date)
 
-        when '/fitness'
-        when '/motivate'
+        when "/motivate"
           values = Motivate.new
           value = values.select_random
           bot.api.send_message(chat_id: message.chat.id, text: "#{value["text"]}", date: message.date)
+        
+        when '/help'
+          reply.text = "
+            /start: Start a new bot
+            /motivate: Gives you different motivational quotes
+            /learn: Teaches you a new french word
+            /stop: End the bot
+          "
+        
         else bot.api.send_message(chat_id: message.chat.id, text: "Invalid entry, #{message.from.first_name}, you need to use  /start,  /stop , /motivate or /joke")
         end
       end
