57739ac3c43ea02c03ca8dadc935bbd0b1fb54fa
Delice Lydia <Delydia84@gmail.com>
2020-11-16 16:21:24 +0200

    Add movies and parfium apis

--- lib/bot.rb
+++ lib/bot.rb
@@ -13,11 +13,14 @@ class Bot
            welcome to the telegram bot created by Delice Lydia, Kindly use: 
                     /start to start the bot,  
                     /stop to end the bot, 
-                    /motivate to get different motivational quote 
-                    /learn to learn a new french word 
+                    /motivate to get different motivational quote
+                    /laughter to get a joke of the day
+                    /learn to learn a new french word
+                    /movies know a new movie to watch
+                    /parfium get your brand new parfium
                     /help to get more info about the bot")
         when "/learn"
-          new_word = %w[lundi mardi mercredi jeudi vendredi samedi dimanche]
+          new_word = %w[salut/hello bonsoir/good,evening comment,tu,vas/how,are,you? je,vais,bien/i,am,fine tu,me,manque/i,miss,you]
           bot.api.send_message(chat_id: message.chat.id, text: "#{new_word.sample.capitalize}!", date: message.date)
 
         when "/stop"
@@ -28,20 +31,30 @@ class Bot
           value = values.select_random
           bot.api.send_message(chat_id: message.chat.id, text: "#{value["text"]}", date: message.date)
 
-        when '/laughter'
+        when "/laughter"
           values = Joke.new
           value = values.make_the_request
-          bot.api.send_message(chat_id: message.chat.id, text: (value['joke']).to_s, date: message.date)
-        
-        when '/help'
-          bot.api.send_message(chat_id: message.chat.id, text: 
-          "
-            /start: Start a new bot
-            /motivate: Gives you different motivational quotes
-            /learn: Teaches you a new french word
-            /stop: End the bot
+          bot.api.send_message(chat_id: message.chat.id, text: (value["joke"]).to_s, date: message.date)
+
+        when "/parfium"
+          new_parfium = %w[j,adore la,vie,est,belle dolce&gabbana mon,guerlain]
+          bot.api.send_message(chat_id: message.chat.id, text: "#{new_parfium.sample.capitalize}!", date: message.date)
+
+        when "/movies"
+          new_movies = %w[jurassic,world avengers the,lion,king tigertail the,assistant hamilton shirley star,is,born the,gentleman]
+          bot.api.send_message(chat_id: message.chat.id, text: "#{new_movies.sample.capitalize}!", date: message.date)
+
+        when "/help"
+          bot.api.send_message(chat_id: message.chat.id, text: "
+          /start to start the bot,  
+          /motivate to get different motivational quote
+          /laughter to get a joke of the day
+          /learn to learn a new french word
+          /movies know a new movie to watch
+          /parfium get your brand new parfium
+          /help to get more info about the bot
+          /stop to end the bot
           ")
-        
         else bot.api.send_message(chat_id: message.chat.id, text: "Invalid entry, #{message.from.first_name}, you need to use  /start,  /stop , /motivate or /joke")
         end
       end
